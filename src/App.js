d2e2a32fe0b1369b0ac8e4e1dfc7275df40f7fdc
Paul Bae <pbae@utexas.edu>
2025-04-23 18:40:53 -0400

    Add quantum entanglement explanation component

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import QuantumExplanation from './components/QuantumExplanation';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Quantum Physics Explained</h1>
+        <QuantumExplanation />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/QuantumExplanation.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const QuantumExplanation = () => {
+  return (
+    <div className="quantum-explanation">
+      <h2>Understanding Quantum Entanglement</h2>
+      <div className="explanation">
+        <p>
+          Imagine you have two pairs of magical socks. When you put one sock in 
+          New York and another in Tokyo, whatever happens to one sock instantly 
+          affects the other sock, no matter how far apart they are!
+        </p>
+        <p>
+          That's similar to quantum entanglement - when two particles become 
+          "entangled," they share a special connection. When you measure one 
+          particle, you instantly know what the other particle is doing, even if 
+          it's on the opposite side of the universe.
+        </p>
+        <p>
+          Scientists use this weird connection in quantum computers and secure 
+          communication systems. It's like having two particles that are always 
+          in sync, no matter where they are!
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default QuantumExplanation;
