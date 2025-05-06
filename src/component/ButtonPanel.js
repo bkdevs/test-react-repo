af7c357a4f68e0f1f310e36b03df7ea6e8ad5adf
Paul Bae <pbae@utexas.edu>
2025-05-06 08:47:45 -0400

    Remove trigonometric buttons (sin, cos, tan) from ButtonPanel, leaving only the square root button on the first row.

--- src/component/ButtonPanel.js
+++ src/component/ButtonPanel.js
@@ -17,9 +17,6 @@ export default class ButtonPanel extends React.Component {
     return (
       <div className="component-button-panel">
         <div>
-          <Button name="sin" clickHandler={this.handleClick} />
-          <Button name="cos" clickHandler={this.handleClick} />
-          <Button name="tan" clickHandler={this.handleClick} />
           <Button name="√" clickHandler={this.handleClick} />
         </div>
         <div>
