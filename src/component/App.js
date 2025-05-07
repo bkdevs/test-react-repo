82388eb0d2062f377cfacecc6180227663710430
mck782 <wjsekfghks@gmail.com>
2025-05-07 18:19:06 -0400

    feat: Add π (pi) button above display and handle insertion of pi value in calculation logic

--- src/component/App.js
+++ src/component/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Display from "./Display";
 import ButtonPanel from "./ButtonPanel";
+import Button from "./Button";
 import calculate from "../logic/calculate";
 import "./App.css";
 
@@ -18,6 +19,9 @@ export default class App extends React.Component {
   render() {
     return (
       <div className="component-app">
+        <div className="pi-button-wrapper" style={{ display: 'flex', justifyContent: 'flex-start', marginBottom: '0.5rem' }}>
+          <Button name="π" clickHandler={() => this.handleClick('π')} />
+        </div>
         <Display value={this.state.next || this.state.total || "0"} />
         <ButtonPanel clickHandler={this.handleClick} />
       </div>
