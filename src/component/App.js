044bb77f06840f91b85b53b73ac7c1cf5cabc9a5
mck782 <wjsekfghks@gmail.com>
2025-05-07 23:31:53 -0400

    Add π (pi) and e buttons above display with handlers
    - Adds constant-buttons above Display in App.js
    - When pressed, these set the display to the value of Math.PI or Math.E
    - CSS styling for .constant-buttons

--- src/component/App.js
+++ src/component/App.js
@@ -15,9 +15,17 @@ export default class App extends React.Component {
     this.setState(calculate(this.state, buttonName));
   };
 
+  handleConstantClick = value => {
+    this.setState({ next: value, total: null, operation: null });
+  };
+
   render() {
     return (
       <div className="component-app">
+        <div className="constant-buttons" style={{ display: 'flex', justifyContent: 'center', margin: '10px 0' }}>
+          <button onClick={() => this.handleConstantClick(Math.PI.toString())}>π</button>
+          <button onClick={() => this.handleConstantClick(Math.E.toString())}>e</button>
+        </div>
         <Display value={this.state.next || this.state.total || "0"} />
         <ButtonPanel clickHandler={this.handleClick} />
       </div>
